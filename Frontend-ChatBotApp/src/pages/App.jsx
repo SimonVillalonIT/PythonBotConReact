05dab8d2d7d39ad0333790613f039b0fb2f74750
SimonVillalonIT <simonvillalon10@gmail.com>
2022-10-15 21:38:23 -0300

    Cambio nashe

--- Frontend-ChatBotApp/src/pages/App.jsx
+++ Frontend-ChatBotApp/src/pages/App.jsx
@@ -9,7 +9,6 @@ export default function App() {
       <IAContextProvider>
         <div className="bg-zinc-900 h-screen font-poppins">
           <header className="w-full fixed bg-zinc-800 h-24 border-b-2 border-white">
-            <img src={Logo} className="h-24" />
           </header>
           <Main />
         </div>
